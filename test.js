d9e8a78d4a51aa76a85a9f529c5ae07f63a6567b
Abhinav Kumar Singh <abhinavkrsingh08@gmail.com>
2026-07-31 09:30:10 +0530

    files added

---
+++ test.js
@@ -0,0 +1,12 @@
+import {sum,add} from "./demo4.js" //use of import and export
+console.log("sum=",sum(1));
+console.log("sum=",sum(1,2,3,4,5));
+console.log("sum=",sum(10,12));
+console.log("sum=",sum(14,2,1,34));
+console.log("sum=",sum(12,3,4));
+
+console.log("add=",add(1));
+console.log("add=",add(1,2,3,4,5));
+console.log("add=",add(10,12));
+console.log("add=",add(14,2,1,34));
+console.log("add=",add(12,3,4));
